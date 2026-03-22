fa9368d1111e850cc68310257b1a6d70221dad43
ljy <438081258@qq.com>
2026-03-22 15:04:21 +0800

    [NEXTAI] Change the minSdk to 31, dynamicLightColorScheme & dynamicLightColorScheme need.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
 
     defaultConfig {
         applicationId = "com.android.nextai"
-        minSdk = 26
+        minSdk = 31
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
