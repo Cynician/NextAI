c6e5dc849fa183ccff6f3fd7321d6216945c61bc
ljy <438081258@qq.com>
2026-03-27 00:16:37 +0800

    [NEXTAI][DEBUG] Fix no android entrypoint problem.

--- app/src/main/java/com/android/nextai/activity/MainActivity.kt
+++ app/src/main/java/com/android/nextai/activity/MainActivity.kt
@@ -4,12 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import com.android.nextai.ui.screen.home.HomeScreen
 import com.android.nextai.ui.theme.NeuronVerseTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,12 +19,4 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
