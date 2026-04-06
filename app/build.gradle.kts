cacc31d2a0379da54dcf9f283f6ea5c8c0ec79ce
ljy <438081258@qq.com>
2026-04-06 16:44:28 +0800

    [NEXTAI][Version] Update to 1.0.3.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         minSdk = 31
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.3"
         ndk {
             abiFilters += listOf("arm64-v8a", "x86_64")
         }
