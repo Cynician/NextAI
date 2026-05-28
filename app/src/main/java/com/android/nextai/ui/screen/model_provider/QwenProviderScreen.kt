5c46fd11d1e3476f0a7ec2b33a3e8592328d36ff
ljy <438081258@qq.com>
2026-05-28 21:39:39 +0800

    [NEXTAI] Fixed issue: after clicking the save button, it would not disappear.

--- app/src/main/java/com/android/nextai/ui/screen/model_provider/QwenProviderScreen.kt
+++ app/src/main/java/com/android/nextai/ui/screen/model_provider/QwenProviderScreen.kt
@@ -179,6 +179,7 @@ fun QwenProviderScreen(
                                     model = initialModel,
                                     isOK = isOK
                                 )
+                                isOK = false
                                 focusManager.clearFocus()
                             }
                         )
