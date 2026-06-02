56def804ca22c8bee669a05534db38ae8022e48a
ljy <438081258@qq.com>
2026-06-02 09:41:19 +0800

    [NEXTAI] Fixed issue: In non-batch selection mode, the "CircleCheckbox" in "SessionHeaderView" can still be clicked.

--- app/src/main/java/com/android/nextai/ui/screen/home/drawer/views/SessionHeaderView.kt
+++ app/src/main/java/com/android/nextai/ui/screen/home/drawer/views/SessionHeaderView.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.visible
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -89,7 +90,8 @@ fun SessionHeaderView(
                 .graphicsLayer {
                     translationY = (-20f) * (1f - transitionProgress)
                     alpha = transitionProgress
-                },
+                }
+                .visible(isBatchSelectMode),
             contentAlignment = Alignment.Center
         ) {
             CircleCheckbox(
