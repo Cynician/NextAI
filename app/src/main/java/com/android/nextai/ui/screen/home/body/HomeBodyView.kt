caf72145b30bc436336e258b5f9ce968ceacdf34
ljy <438081258@qq.com>
2026-06-02 15:01:18 +0800

    [NEXTAI] Fixed issue: Keyboard pop-up blocks content issues.

--- app/src/main/java/com/android/nextai/ui/screen/home/body/HomeBodyView.kt
+++ app/src/main/java/com/android/nextai/ui/screen/home/body/HomeBodyView.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -162,6 +163,7 @@ fun HomeBodyView(
                 modifier = Modifier
                     .fillMaxSize()
                     .nestedScroll(nestedScrollConnection)
+                    .imePadding()
                     .padding(12.dp),
                 contentPadding = PaddingValues(
                     top = paddingValues.calculateTopPadding() + 8.dp,
